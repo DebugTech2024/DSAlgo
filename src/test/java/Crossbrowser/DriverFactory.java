c3779843446433efc93dc54fce96ec65990efe17
kowsika34 <kowsika.shrimv@gmail.com>
2024-07-16 00:04:08 -0400

    committed

---
+++ src/test/java/Crossbrowser/DriverFactory.java
@@ -0,0 +1,58 @@
+package Crossbrowser;
+
+import java.time.Duration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverFactory {
+	public WebDriver driver;
+	public static ThreadLocal<WebDriver>tldriver=new ThreadLocal<>();
+	private static Logger logger=LogManager.getLogger(DriverFactory.class);
+	
+	public WebDriver init_driver(String browser) {
+		logger.info("browser value is");
+		if (browser.equals("chrome")){
+			WebDriverManager.chromedriver().setup();
+			tldriver.set(new ChromeDriver());
+		}
+		else if(browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			tldriver.set(new FirefoxDriver());
+		}
+		else if(browser.equals("safari")) {
+			WebDriverManager.safaridriver().setup();
+			tldriver.set(new SafariDriver());
+		}
+		else {
+			logger.info("please pass the correct browservalue:"+browser);
+		}
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().window().maximize();
+		getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		return getDriver();
+		
+	}
+
+	public static synchronized WebDriver getDriver() {
+		// TODO Auto-generated method stub
+		return tldriver.get();
+	}
+
+	public void closeallDriver() {
+		// TODO Auto-generated method stub
+		driver.close();
+	}
+
+
+	
+
+	
+}
---
+++ src/test/java/Hooks/Apphooks.java
@@ -0,0 +1,41 @@
+package Hooks;
+
+import java.util.Properties;
+
+
+import org.openqa.selenium.WebDriver;
+
+import Crossbrowser.DriverFactory;
+import Utility.ConfigFileReader;
+import Utility.LoggerLoad;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Apphooks {
+	private static DriverFactory driverfactory;
+	public WebDriver driver;
+	private static ConfigFileReader configFileReader;
+	static String prop;
+	
+
+	@Before
+	public static void getProperty() {
+		configFileReader =new ConfigFileReader();
+		try {
+			prop=configFileReader.getBrowser();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
+}
+	@After
+	public static void after() {
+		LoggerLoad.info("closingDriver");
+		
+			// driverfactory.closeallDriver();
+		
+	}
+
+}
