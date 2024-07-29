cdb27093e0168065e5a970aad329c692e5d61a57
kowsika34 <kowsika.shrimv@gmail.com>
2024-07-28 20:33:07 -0400

    commited loginexcel,pom,testrunner

--- src/test/java/runner/testrunner.java
+++ src/test/java/runner/testrunner.java
@@ -1,28 +1,27 @@
 package runner;
 
-import org.junit.runner.RunWith;
+
+
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
-
 import Utility.ConfigFileReader;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
 
 @SuppressWarnings("unused")
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features",
 glue={"StepDefinition","Hooks"},
 monochrome=true,
 plugin= {"pretty","junit:target/JUnitReports/report.xml",
 		"json:target/JSONReports/report.json",
 		"html:target/HTMLReports",
-		"com.aventstack.extentreports.Cucumber.adpter.ExtentCucumberAdpter:",
-		"time line:test-output-thread/"
+		//"com.aventstack.extentreports.Cucumber.adpter.ExtentCucumberAdpter:",
+		//"time line:test-output-thread/"
 		}
 )
 
